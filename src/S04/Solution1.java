0f5961d86d34d0b80d9d3e27b3f01c7200b10a4b
austenYang <austenyad@gmail.com>
2020-12-23 18:54:19 +0800

    Solution1

--- src/S04/Solution1.java
+++ src/S04/Solution1.java
@@ -1,41 +1,39 @@
 package S04;
 
 
-
-
 public class Solution1 {
-
-
     /**
      * 请实现一个函数，把字符串中的每个空格换成 "%20"。
      * 例如 "We   are happy"，则输出 "We%20are%20happy"
+     * // 从后往前 替换空格
      *
      * @param str
      * @return
      */
     public static String transStr(String str) {
-        int count = 0;
+
         char[] chars = str.toCharArray();
+        int whiteCount = 0;
         for (int i = 0; i < chars.length; i++) {
-            if (chars[i] == 32) {
-                count++;
+            if (' ' == chars[i]) {
+                whiteCount++;
             }
         }
-        // 空格原来是一个字符位置，替换成 %20 后增加2个空格
-        int strLength = str.length() + count * 2;
-        char[] newChars = new char[strLength];
-        for (int i = 0; i < chars.length; i++) {
-            newChars[i] = chars[i];
-        }
+        char[] newChars = new char[chars.length + whiteCount * 2];
+        System.arraycopy(chars, 0, newChars, 0, chars.length);
+        int p2 = newChars.length - 1;
+        int p1 = newChars.length - 1 - whiteCount * 2;
 
-
-        for (int i = 0; i < newChars.length; i++) {
-            if (newChars[i] == ' ' && count > 0) {
-                System.arraycopy(newChars, i + 1, newChars, i + 3, newChars.length  - count * 2);
-                count--;
-                newChars[i] = '%';
-                newChars[i + 1] = '2';
-                newChars[i + 2] = '0';
+        while (p1 != p2) {
+            newChars[p2] = newChars[p1];
+            p1--;
+            p2--;
+            if (newChars[p1] == ' ') {
+                p1--;
+                newChars[p2] = '0';
+                newChars[--p2] = '2';
+                newChars[--p2] = '%';
+                p2--;
             }
         }
         return new String(newChars);
